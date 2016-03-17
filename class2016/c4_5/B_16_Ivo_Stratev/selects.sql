3f24fe325662778756c033650cb9c9af9ebd79f0
NoHomey <ivo_stratev@abv.bg>
2016-03-17 10:35:32 +0200

    outer

--- class2016/c4_5/B_16_Ivo_Stratev/selects.sql
+++ class2016/c4_5/B_16_Ivo_Stratev/selects.sql
@@ -11,3 +11,15 @@ inner join Events on Events.id = Events_Groups.Event_id;
 select Users.Name, Groups.Name from Users
 left join Groups_Users on Groups_Users.User_id = Users.id
 left join Groups on Groups.id = Groups_Users.Group_id;
+
+select Users.Name, Groups.Name from Users
+right join Groups_Users on Groups_Users.User_id = Users.id
+right join Groups on Groups.id = Groups_Users.Group_id;
+
+select Users.Name, Groups.Name from Users
+left join Groups_Users on Groups_Users.User_id = Users.id
+left join Groups on Groups.id = Groups_Users.Group_id
+union 
+select Users.Name, Groups.Name from Users
+right join Groups_Users on Groups_Users.User_id = Users.id
+right join Groups on Groups.id = Groups_Users.Group_id;
