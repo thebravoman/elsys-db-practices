add55b2d7f25e7fc4c9bb43ec9ad308c1e2507df
valentinvarbanov <valentin.varbanov@gmail.com>
2016-03-17 10:07:34 +0200

    Update selects.sql

--- class2016/c4_5/B_04_Valentin_Varbanov/selects.sql
+++ class2016/c4_5/B_04_Valentin_Varbanov/selects.sql
@@ -5,3 +5,13 @@ INNER JOIN Groups_Users_
 INNER JOIN Groups_ 
   ON Groups_.id = Groups_Users_.group_id;
 
+SELECT Users_.id, Users_.name 
+FROM Users_ 
+INNER JOIN Groups_Users_ 
+	ON Users_.id = Groups_Users_.user_id 
+INNER JOIN groups_ 
+	ON Groups_.id = Groups_Users.group_id 
+INNER JOIN Groups_Events_ 
+	ON Groups_.id = Groups_Events_.group_id 
+INNER JOIN Events_
+	ON Events_.id = Groups_Events_.event_id;
