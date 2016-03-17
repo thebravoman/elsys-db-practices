1ed71f917409f3f26e318278985aa2e51be587d2
NoHomey <ivo_stratev@abv.bg>
2016-03-17 09:09:39 +0200

    creates and inserts

---
+++ class2016/c4_5/B_16_Ivo_Stratev/creates.sql
@@ -0,0 +1,41 @@
+drop database calendar;
+create database calendar;
+use calendar;
+
+create table Users (
+    id integer not null primary key auto_increment,
+    Name varchar(20),
+    Email varchar(20),
+    Age date
+);
+
+create table Groups (
+    id integer not null primary key auto_increment,
+    Name varchar(20),
+    Date_Founded_on date
+);
+
+create table Events (
+    id integer not null primary key auto_increment,
+    Date_ date,
+    Name varchar(20),
+    Location varchar(100)
+);
+
+create table Groups_Users (
+    id integer not null primary key auto_increment,
+    User_id integer not null,
+    Group_id integer not null
+);
+
+alter table Groups_Users add foreign key (User_id) references Users(id);
+alter table Groups_Users add foreign key (Group_id) references Groups(id);
+
+create table Events_Groups (
+    id integer not null primary key auto_increment,
+    Event_id integer not null,
+    Group_id integer not null
+);
+
+alter table Events_Groups add foreign key (Event_id) references Events(id);
+alter table Events_Groups add foreign key (Group_id) references Groups(id);
---
+++ class2016/c4_5/B_16_Ivo_Stratev/inserts.sql
@@ -0,0 +1,14 @@
+insert into Users (Name, Email, Age) values("Pesho", "pesho@gmail.com", "2015-05-12");
+insert into Users (Name, Email, Age) values("Gosho", "gosho@gmail.com", "2014-06-11");
+insert into Users (Name, Email, Age) values("Sasho", "sasho@gmail.com", "2013-07-10");
+insert into Users (Name, Email, Age) values("Tosho", "tosho@gmail.com", "2016-04-13");
+
+insert into Groups (Name, Date_Founded_on) values("Pesho", "2015-05-12");
+insert into Groups (Name, Date_Founded_on) values("Gosho", "2014-06-11");
+insert into Groups (Name, Date_Founded_on) values("Sasho", "2013-07-10");
+insert into Groups (Name, Date_Founded_on) values("Tosho", "2016-04-13");
+
+insert into Events (Date_, Name, Location) values("2015-05-12", "Pesho", "Sofia");
+insert into Events (Date_, Name, Location) values("2014-08-20", "Gosho", "Varna");
+insert into Events (Date_, Name, Location) values("2013-05-21", "Tosho", "Sofia");
+insert into Events (Date_, Name, Location) values("2012-02-25", "Sasho", "Plovdiv");
