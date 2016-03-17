c1892fd34ab3bd42d67e8cf08e4f87565199d2e0
Emiliyan <emiliqn_sokolov@abv.bg>
2016-03-17 09:55:36 +0200

    Added files via upload

---
+++ class2016/c4_5/12b_15_Emiliyan_Sokolov/Inserts.sql
@@ -0,0 +1,23 @@
+insert into Users (name, email, age) values ("Pesho", "pesho@gmail.com", 22);
+insert into Users (name, email, age) values ("Asen", "asen@gmail.com", 18);
+insert into Users (name, email, age) values ("Momo", "momo@gmail.com", 25);
+insert into Users (name, email, age) values ("Riki", "riki@gmail.com", 30);
+insert into Users (name, email, age) values ("Naiden", "naiden@gmail.com", 38);
+
+insert into Groups (name, date_founded_on) values ("Pesho", "2009-04-05");
+insert into Groups (name, date_founded_on) values ("Asen", "2008-06-11");
+insert into Groups (name, date_founded_on) values ("Momo", "2012-04-09");
+insert into Groups (name, date_founded_on) values ("Riki", "2013-18-08");
+insert into Groups (name, date_founded_on) values ("Naiden", "2010-22-10");
+
+insert into Events (name, data, location) values ("Pesho", "2010-05-12", "Pernik");
+insert into Events (name, data, location) values ("Asen", "2010-14-08", "Asenovgrad");
+insert into Events (name, data, location) values ("Momo", "2012-06-10", "Sofia");
+insert into Events (name, data, location) values ("Riki", "2014-19-04", "Blagoevgrad");
+insert into Events (name, data, location) values ("Naiden", "2011-27-12", "Plovdiv");
+
+
+
+
+
+
