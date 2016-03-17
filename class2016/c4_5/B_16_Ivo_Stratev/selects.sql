49ebe4ee2e50731c0dae6a8a3169fc8d4c9f0a7b
NoHomey <ivo_stratev@abv.bg>
2016-03-17 10:17:12 +0200

    left join

--- class2016/c4_5/B_16_Ivo_Stratev/selects.sql
+++ class2016/c4_5/B_16_Ivo_Stratev/selects.sql
@@ -7,3 +7,7 @@ inner join Groups_Users on Groups_Users.User_id = Users.id
 inner join Groups on Groups.id = Groups_Users.Group_id
 inner join Events_Groups on Events_Groups.Group_id = Groups_Users.Group_id
 inner join Events on Events.id = Events_Groups.Event_id;
+
+select Users.Name, Groups.Name from Users
+left join Groups_Users on Groups_Users.User_id = Users.id
+left join Groups on Groups.id = Groups_Users.Group_id;
