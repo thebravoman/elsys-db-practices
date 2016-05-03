b598a8bba5adbcbc33f79ab5b497536abb2b0272
Kiko <k.mitov@robopartans.com>
2016-05-03 12:33:22 +0300

    Removing david folder

--- exam2016/exam20160428/b/David_Georgiev_B_12/selects1.sql
+++
@@ -1,5 +0,0 @@
-/*Which are the Article_11(s) for a given User
-alter table User add column category_id int unique;
-alter table Article_11 add column category_id int;
-*/
-SELECT id FROM Article_11 WHERE Article_11.id IN (SELECT category_id FROM User WHERE User.id = 1);
